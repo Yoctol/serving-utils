b3f8ec98a6b1b4b0288c1826a43943b013892a46
cph <stegben.benjamin@gmail.com>
2018-07-11 11:41:45 +0800

    modify installation setup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 from setuptools import setup
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 
 readme = Path(__file__).parent.joinpath('README.md')
 if readme.exists():
@@ -9,6 +11,21 @@ if readme.exists():
 else:
     long_description = '-'
 
+
+class BuildPackageProtos(install):
+    def run(self):
+        install.run(self)
+        from grpc.tools import command
+        command.build_package_protos('')
+
+
+class BuildPackageProtosDevelop(develop):
+    def run(self):
+        develop.run(self)
+        from grpc.tools import command
+        command.build_package_protos('')
+
+
 setup(
     name='serving-utils',
     version='0.1.0',
@@ -16,11 +33,22 @@ setup(
     long_description=long_description,
     python_requires='>=3.6',
     packages=[
-        'serving-utils',
+        'serving_utils',
     ],
     author='Po-Hsien Chu',
     author_email='cph@yoctol.com',
     url='https://github.com/samuelcolvin/arq',
     license='MIT',
-    install_requires=[],
+    setup_requires=[
+        'cython',
+    ],
+    install_requires=[
+        'grpcio-tools',
+        'aiogrpc>=1.5',
+        'numpy>=1.14.0',
+    ],
+    cmdclass={
+        'install': BuildPackageProtos,
+        'develop': BuildPackageProtosDevelop,
+    },
 )
