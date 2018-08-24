9f78fde13d01574b4f6c0ac8449ba3ee26bae79f
cph <stegben.benjamin@gmail.com>
2018-08-24 23:08:36 +0800

    v0.5.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ class BuildPackageProtosDevelop(develop):
 
 setup(
     name='serving-utils',
-    version='0.4.6',
+    version='0.5.0',
     description='Some utilities for tensorflow serving',
     long_description=long_description,
     python_requires='>=3.6',
