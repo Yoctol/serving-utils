eddee1e851d6223373426aeeb329b0a7ed93b686
cph <stegben.benjamin@gmail.com>
2019-02-27 11:49:00 +0800

    add loader structure

---
+++ serving_utils/loader.py
@@ -0,0 +1,10 @@
+class Loader:
+
+    def __init__(self, saved_model_path):
+        pass
+
+    def load_to_current_graph(self, input_map):
+        pass
+
+    def _get_signature_defs(self):
+        pass
